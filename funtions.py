a012d136ed85b6b79fda9a1a371c9ba490f61131
ESPINOZA EDWIN E <eesp1801@education.mpls.k12.mn.us>
2017-09-08 10:04:03 -0500

    add files

---
+++ funtions.py
@@ -0,0 +1,10 @@
+def hello():
+    print " hello world "
+    
+def hellovar():
+    a = "Hi"
+    print a
+    
+def helloinput():
+    name = raw_input ("whats your name ?")
+    print "hello" + name 
